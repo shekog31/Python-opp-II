ce90d3fee2af49b6d42adc58a9c5af2c73c05075
shekog31 <shekhar.tampa@gmial.com>
2024-09-21 19:58:36 -0400

    Logical Operators

--- and/or.py
+++ and/or.py
@@ -1,1 +1,32 @@
-print("hi")
+a=36
+b=-29
+c=68
+d=0
+
+if a and b and c and d:
+    print("All statements are true")
+else:
+    print("Statement is false")
+    
+e=9
+f=6
+g=-7
+h=3
+
+if e>0 or f>0 or g>0 or h>0:
+    print("All statements are true")
+    print("Or some statements are true")
+else:
+    print("Statements are false")
+    
+w=76
+x=38
+y=0
+z=45
+
+if w and x and y and z:
+    print("All statements are true")
+elif w>0 or x>0 or z>0:
+    print("w,x,z are true y is not")
+else:
+    print("All statements are false")
